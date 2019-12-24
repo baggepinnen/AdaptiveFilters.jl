16ac38afcc09578f2de74f28209d183a52f28426
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2019-12-24 11:59:39 +0800

    docstring

--- src/AdaptiveFilters.jl
+++ src/AdaptiveFilters.jl
@@ -10,6 +10,8 @@ export adaptive_filter, OMAP, MSPI, OMAS, ADAM, ExponentialWeight, EqualWeight
 Filters `y` with an adaptive AR (only poles) filter with specified order.
 Returns `yh` which is the predicted output from an adaptive line enhancer (ALE). If your noise is wideband and signal narrowband, `yh` is your desired filtered signal. If the noise is narrowband and the signal is wideband, then `y-yh` is your desired filtered signal.
 
+The first `order` samples of `yh` will be copies of `y`. The signals will thus have the same length.
+
 #Arguments:
 - `alg`: Stochastic approximation algorithm or weight function. Examples: `OMAP, MSPI, OMAS, ADAM, ExponentialWeight, EqualWeight`
 - `y`: Input signal
