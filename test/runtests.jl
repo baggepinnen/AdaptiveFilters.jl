bd6dd537705f62898bf0eee2355038eb44fcdc5c
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2019-12-26 10:27:57 +0800

    specify stuff in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,15 +3,12 @@ using Test, Statistics
 
 @testset "AdaptiveFilters.jl" begin
     y = sin.(0:0.1:100)
-    yh = adaptive_filter(y)
+    yh = adaptive_filter(y, lr=0.01, order=4)
 
-    @test mean(abs2, y-yh) < 1e-4
+    @test mean(abs2, y-yh) < 1e-3
 
-    yh = adaptive_filter(y, lr = 0.99)
-    @test 1e-3 < mean(abs2, y-yh) < 1e-2
-
-    yh = adaptive_filter(y, lr = 0.01)
-    @test mean(abs2, y-yh) < 1e-7
+    yh = adaptive_filter(y, lr = 0.99, order=4)
+    @test 1e-2 < mean(abs2, y-yh) < 1e-1
 
     yh = adaptive_filter(y, ExponentialWeight, order=2, lr = 0.01)
     @test mean(abs2, y-yh) < 1e-4
